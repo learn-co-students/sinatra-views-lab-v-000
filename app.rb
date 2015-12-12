e1c7d2a02d16f21e0d0ba9ffc0c5fa11b587bf27
jakebrady5 <jakebrady5@gmail.com>
2015-12-11 21:35:23 -0500

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,28 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+  get '/hello' do
+    erb :hello
+  end
+
+  get '/goodbye' do
+    erb :goodbye
+  end
+
+  get '/dogs' do
+    erb :dogs
+  end
+
+  get '/cats' do 
+    erb :cats
+  end
+
+  get '/music' do
+    erb :music
+  end
+
+  get '/date' do
+    erb :date
+  end
 
 end
