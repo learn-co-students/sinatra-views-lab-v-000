3846e34d9df66dafcd03ddc1a36e68a735754252
febbraiod <donedesign1@gmail.com>
2016-01-27 13:50:09 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,22 @@
 class App < Sinatra::Base
 
 	get '/' do
-		erb :index
+		"hi don"
 	end
+
+  get '/hello' do
+    erb :hello
+  end
+
+  get '/goodbye' do
+    @name = "Joe"
+    erb :goodbye
+  end
+
+  get '/date' do
+    @date = Time.now.strftime("%A, %B %d, %Y")
+    erb :date
+  end
 
 
 end
