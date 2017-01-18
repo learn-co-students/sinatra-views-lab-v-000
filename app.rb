2b81944715a160ffa2d5a0da90a35d6584501248
warrend <warrend555@gmail.com>
2017-01-18 17:34:43 -0500

    done

--- app.rb
+++ app.rb
@@ -4,5 +4,16 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+  get '/hello' do
+    erb :hello
+  end
 
+  get '/date' do
+    erb :date
+  end
+
+  get '/goodbye' do
+    @name = 'Joe'
+    erb :goodbye
+  end
 end
