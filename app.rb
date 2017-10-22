61cf58ebfda101be7560b3cf5429a96dfc34e955
Jacilyn Hayden <jacihayden@gmail.com>
2017-10-22 19:18:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,20 @@
+# require_relative 'config/environment' ** when does this get used? was in last lab, not this one..
 class App < Sinatra::Base
 
 	get '/' do
 		erb :index
 	end
 
+	get '/hello' do
+	  erb :hello
+	end
+
+	get '/goodbye' do
+	  erb :goodbye
+	end
+
+	get '/date' do
+		erb :date
+	end
 
 end
