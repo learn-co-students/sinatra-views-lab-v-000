65247a3821b1af1d72881b004237cd2851c868a5
Aspen James <aspenjames@tqca.org>
2018-04-29 20:22:40 -0700

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,18 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get '/hello' do
+		erb :hello
+	end
+
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@now = DateTime.now
+		erb :date
+	end
 
 end
