f067d797e06c3836c1fc9a64e67b82051f730721
lrh2daizzo <herrera.lrh@gmail.com>
2021-01-14 11:39:01 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 class App < Sinatra::Base
-
 	get '/hello' do
-		erb :hello 
+		erb :hello
 	end
-	
+
 	get '/goodbye' do
+		@name = 'Joe'
 		erb :goodbye
 	end
 
 	get '/date' do
-		erb :date 
+		erb :date
 	end
 end
